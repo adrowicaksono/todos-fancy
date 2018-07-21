7fb641daa4eef1da89de0c1fed77c0325452e3aa
adrowicaksono <adrowicaksono@gmail.com>
2018-07-21 17:01:26 +0700

    migrate to bin

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
-const port = 3000
-const express = require('express')
-const app = express()
-const cors = require('cors')
 require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -30,6 +38,20 @@ app.use('/user', userRouter)
 app.use('/auth', authRouter)
 app.use('/task', taskRouter)
 
-app.listen(3000, function(){
-    console.log(`listen ${port}`)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err.message);
+});
+
+module.exports = app;
